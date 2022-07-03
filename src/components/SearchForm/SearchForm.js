385308fc9dc65b300d775e98fe4b0b643711a4ae
flare-s <webflarez@gmail.com>
2022-07-03 20:42:24 +0100

    Added the list of suggestons to the UI

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -2,11 +2,16 @@ import { useState } from "react";
 import styles from "./SearchForm.module.css";
 import SuggestionsList from "./SuggestionsList/SuggestionsList";
 const SearchForm = () => {
+    const [value, setValue] = useState("");
+
+    const handleChange = (e) => {
+        setValue(e.target.value);
+    }
     return (
         <form className={styles["search-form"]}>
-            <input type="text" name="search" className={styles["search-input"]} placeholder="Enter acronym"/>
+            <input type="text" name="search" className={styles["search-input"]} value={value} onChange={(e) => handleChange(e)} placeholder="Enter acronym"/>
             <button className={styles["search-button"]}>Search</button>
-            <SuggestionsList/>
+            <SuggestionsList value={value} setValue={setValue}/>
         </form>
     )
 }
--- src/components/SearchForm/SuggestionsList/SuggestionsList.js
+++ src/components/SearchForm/SuggestionsList/SuggestionsList.js
@@ -1,8 +1,20 @@
 import styles from "./SuggestionsList.module.css";
 import data from "../../../data";
 const SuggestionsList = (props) => { 
+    const {value, setValue} = props;
+
+    const handleClick = (val) => {
+        setValue(val);
+    }
     return (
         <ul className={styles["suggestions-list"]}>
+            {data
+            .filter(el => { 
+                let key = Object.keys(el)[0].toLowerCase();
+                return value.trim() && key.startsWith(value.toLowerCase()) && key !== value.toLowerCase()
+            })
+            .slice(0, 5)
+            .map((el, i) => <li key={i} onClick={(e) => handleClick(e.target.textContent)} className={styles.suggestion}>{Object.keys(el)[0]}</li>)}
         </ul>
     )
 }
